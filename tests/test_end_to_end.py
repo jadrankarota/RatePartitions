58bad89bb74410010d029bca59341233cc005b1a
carlosp420 <mycalesis@gmail.com>
2018-05-12 15:30:18 -0500

    more integration tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -14,12 +14,20 @@ class TestRatePartitions(TestCase):
         Test that the output data of running the rate_partitions script is the
         expected file contents
         """
-        infile = self.test_data_path + "AS1-1.phy_r8s.txt"
-        outfile = self.test_data_path + "AS1-1.phy_r8s.txt_2.5.txt"
-        divnum = 2.5
-        result = run(infile, divnum)
+        files = [
+            ("AS1-1.phy_r8s.txt", "AS1-1.phy_r8s.txt_2.5.txt"),
+            ("AS1-3.phy_r8s.txt", "AS1-3.phy_r8s.txt_2.5.txt"),
+            ("AS1-4.phy_r8s.txt", "AS1-4.phy_r8s.txt_2.5.txt"),
+        ]
+        for file_pair in files:
+            input_file = file_pair[0]
+            expected_file = file_pair[1]
+            infile = self.test_data_path + input_file
+            outfile = self.test_data_path + expected_file
+            divnum = 2.5
+            result = run(infile, divnum)
 
-        with open(outfile) as handle:
-            expected_result = handle.read()
-            self.assertEqual(expected_result, result)
+            with open(outfile) as handle:
+                expected_result = handle.read()
+                self.assertEqual(expected_result, result)
 
